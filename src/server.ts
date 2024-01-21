be48b3d64e07a0c12cc032ae1aa7282ac05e6289
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-21 13:14:20 +0200

    *

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,25 @@ import remarkRehype from 'remark-rehype';
 import { MdxBundlerResultType, MdxSourceType, MdxBundlerOptions } from './types';
 import remarkParse from 'remark-parse';
 import rehypeStringify from 'rehype-stringify';
-
+import rehypeParse from 'rehype-parse';
+import { mdxJsxFlowElement } from './mdx-jsx-flow-element';
 export async function MdxServerUnifiedBundler(
   source: MdxSourceType,
   options?: MdxBundlerOptions,
 ): Promise<MdxBundlerResultType> {
   const unifiedOptions = options?.unifiedOptions ?? [];
-  const processor = unified().use(remarkParse).use(mdx).use(remarkRehype);
+  const processor = unified()
+    .use(remarkRehype, {
+      allowDangerousHtml: true,
+      handlers: {
+        mdxJsxFlowElement,
+      },
+    })
+    .use(rehypeParse, {
+      fragment: true,
+    })
+    .use(remarkParse)
+    .use(mdx);
   for (const unifiedOption of unifiedOptions) {
     processor.use(unifiedOption);
   }
