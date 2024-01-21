80e2189891eace6d8c64a623c15aa13bdcee710b
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-21 13:14:20 +0200

    *

--- src/types.ts
+++ src/types.ts
@@ -5,11 +5,9 @@ import { VFileWithOutput } from 'unified/lib';
 export type MdxBundlerOptions = {
   unifiedOptions?: Plugin[];
 };
-export type MdxBundlerResultType<R extends JSX.Element | undefined = undefined> = VFileWithOutput<R>;
-type MDXComponentType<T = any> = {
-  [key: string]: T;
-};
-export type MDXComponentPropsType<T = any> = {
-  components: MDXComponentType<T>;
+export type MdxBundlerResultType = VFileWithOutput<any>;
+
+export type MDXComponentPropsType = {
+  components: Record<string, React.ComponentType>;
   vfile: MdxBundlerResultType;
 };
